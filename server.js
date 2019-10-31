06b7c833b371e8c688e77bf2c658d383cadaf41b
Marija Savija <marija.savija.148@gmail.com>
2019-11-01 00:18:51 +0100

    User login

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/api/users"));
-
+app.use("/api/auth", require("./routes/api/auth"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
